c410393cbe634316db485c91b93774ae1ee324cc
Sir-Newton <105282831+Sir-Newton@users.noreply.github.com>
2022-05-10 14:23:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,15 @@ export default function Home() {
     insights from data is known as data visualization. Data visualization is primarily used for data cleansing, exploration, and discovery, as 
     well as for conveying outcomes to Business stakeholders. It can be used as a tool for data pre-processing as well as gaining insights into the data. 
     The key to effective communication of outcomes is having the correct selection of visuals for diverse data types and business scenarios. In this project, 
-    we will be discussing business insights that can be drawn from the data gotten from a Superstore Giant in the United States of America
+    we will be discussing business insights that can be drawn from the data gotten from a Superstore Giant in the United States of America.
     
     
-    
+    THE DATASET
+The Superstore dataset was gotten from  
+    <a href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true"> Kaggle </a>
+    (https://www.kaggle.com/datasets/vivek468/superstore-dataset-final ) and it about a 
+    superstore giant looking to understand which product, categories, regions and customer segment they focus on or be wary of. The dataset contains 9994 rows of records and 21 columns of attributes with no missing values.
+
     
     
     </h4>
@@ -32,7 +37,7 @@ export default function Home() {
                 <h4></h4>
                 
                 
-       <link rel="icon" href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true" /> 
+//        <link rel="icon" href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true" /> 
     <a href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true"> Link to the dataset </a>
         </p>
       </main>
