428c9a4904ef576349f626f086fb144e5969bf62
Sir-Newton <105282831+Sir-Newton@users.noreply.github.com>
2022-05-10 14:15:16 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,15 @@ export default function Home() {
       <main>
         <Header title="BBsTeams App!" />
         <p className="description">
+        
+                <h3>GROUP 4 INTERACTIVE VISUALZATION ASSESMENT</h3>
+                <h4>  </h4>
+                <h4>WHAT IS INTERACTIVE VISUALIZATION</h4>
+                <h4></h4>
+                <h4></h4>
+                <h4></h4>
+                
+                
        <link rel="icon" href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true" /> 
     <a href="https://docs.google.com/spreadsheets/d/1vGWkF3vBHJc-kG7uIeD5_A6G43w50ISs/edit?rtpof=true"> Link to the dataset </a>
         </p>
